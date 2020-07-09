3c87373aa09be9710f43a000032f2bb987667c31
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-09 15:09:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,12 +10,11 @@ array_integers = [
  row_index = 0
   while row_index < array_integers.length do
     element_index = 0
-    even_values = 0
+    even_values = 2
     while element_index < array_integers[row_index].length do
       p array_integers[row_index][element_index]
+      if array_integers[row_index][element_index] 
       element_index += 1
-      if array_integers[row_index][element_index] % 2 = 0
-        p array_integers[row_index][element_index]
     end
     row_index += 1
   end
