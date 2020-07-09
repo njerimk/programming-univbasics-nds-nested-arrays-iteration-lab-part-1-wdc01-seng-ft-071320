438792c02bc7fb63b00be52d46a68f3c5b434a68
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-09 15:28:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,9 +10,8 @@ src = [
  row_index = 0
   while row_index < src.count do
     element_index = 0
-    even_values = 2
     while element_index < src[row_index].count do
-      p src[row_index][element_index]
+        src[row_index][element_index]
         if src[row_index][element_index].even?
           p src[row_index][element_index]
       end
